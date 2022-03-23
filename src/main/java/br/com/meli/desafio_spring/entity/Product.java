6fbfa5b7f6bf560a990b495929ba5785d1af5edd
Luis Olimpio <luis.olimpio@mercadolivre.com>
2022-03-23 15:42:00 -0300

    setup: create product attributes

--- src/main/java/br/com/meli/desafio_spring/entity/Product.java
+++ src/main/java/br/com/meli/desafio_spring/entity/Product.java
@@ -1,4 +1,21 @@
 package br.com.meli.desafio_spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    private Long productId;
+    private String name;
+    private String category;
+    private String brand;
+    private BigDecimal price;
+    private int quantity;
+    private boolean freeShipping;
+    private String prestige;
 }
