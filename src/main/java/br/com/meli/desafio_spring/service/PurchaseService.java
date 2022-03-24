fc05df020b5d11b77a012eab2624ecc32da03445
arthur-meli <arthur.souza@mercadolivre.com>
2022-03-24 17:29:07 -0300

    feat: validator exceptions

--- src/main/java/br/com/meli/desafio_spring/service/PurchaseService.java
+++ src/main/java/br/com/meli/desafio_spring/service/PurchaseService.java
@@ -17,10 +17,12 @@ public class PurchaseService {
     @Autowired
     private ArticleService articleService;
 
+
     public Purchase savePurchase(List <ArticlePurchaseDTO> articlesPurchaseDTO){
 
         List<Article> articlesList = new ArrayList<>();
 
+        checkOperation(articlesPurchaseDTO);
         for (ArticlePurchaseDTO a:articlesPurchaseDTO) {
             articlesList.add(articleService.findByID(a.getProductId()));
         }
@@ -29,5 +31,27 @@ public class PurchaseService {
     }
 
 
+    public void checkOperation(List <ArticlePurchaseDTO> articlesPurchaseDTO){
+        List <Long> listIdsError = new ArrayList<>();
+        List <String> messageError = new ArrayList<>();
+
+        for (ArticlePurchaseDTO a:articlesPurchaseDTO) {
+
+            if(articleService.findByID(a.getProductId()) == null){
+                listIdsError.add(a.getProductId());
+                messageError.add(" ID: " + a.getProductId() + "Nao cadastrado; ");
+            }
+            if (articleService.findByID(a.getProductId()).getQuantity() > a.getQuantity()){
+                listIdsError.add(a.getProductId());
+                messageError.add("Quantidade para o ID: " + a.getProductId() + "invalida");
+            }
+        }
+        if (!listIdsError.isEmpty())
+            throw new BadRequest (messageError);
+
+    }
+
+    // Valida se ID exist
+    // Valida se Quantity
 
 }
